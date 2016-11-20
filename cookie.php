ca34483a9600e0854ea6d4931e39700bc5eaed08
Nikhil Kanoor <nk352@afsconnect2.njit.edu>
2016-11-20 11:36:08 -0500

    cookie

---
+++ cookie.php
@@ -0,0 +1,7 @@
+<?php  
+setrawcookie();  
+print_r($_COOKIE);  
+/*small text files loaded from a server to a client computer storing some information regarding the
+client computer, so that when the same page from the server is visited by the user, necessary information
+can be collected from the cookie itself*/
+?>  
